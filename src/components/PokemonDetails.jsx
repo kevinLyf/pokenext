957e668941928cb06718d00508ebd250ddffd677
Kevin Feitosa <75489626+kevinLyf@users.noreply.github.com>
2023-04-09 16:53:28 -0300

    fix(card details): remove type bug

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import capitalize from "../hooks/capitalize";
 import getType from "@/hooks/getType";
-import Image from "next/image";
 
 function PokemonDetails({ pokemonName, isSearch, setIsSearch }) {
   const [pokemonData, setPokemonData] = useState(null);
@@ -10,11 +9,7 @@ function PokemonDetails({ pokemonName, isSearch, setIsSearch }) {
     if (isSearch) {
       fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
         .then((response) => response.json())
-        .then((data) => {
-          setPokemonData(data);
-          console.log(data.types[0].type.name);
-          console.log(data);
-        })
+        .then((data) => setPokemonData(data))
         .catch((error) => console.error(error));
 
       setIsSearch(false);
@@ -25,8 +20,17 @@ function PokemonDetails({ pokemonName, isSearch, setIsSearch }) {
     return <div>Loading...</div>;
   }
 
+  const types = pokemonData.types.map((type) => (
+    <p
+      className={`pokemon-card-type ${getType(type.type.name)}`}
+      key={type + pokemonData.id}
+    >
+      {type.type.name}
+    </p>
+  ));
+
   return (
-    <div className="w-full ">
+    <div className="w-full">
       <div
         className={`pokemon-card ${getType(pokemonData.types[0].type.name)}`}
       >
@@ -35,20 +39,33 @@ function PokemonDetails({ pokemonName, isSearch, setIsSearch }) {
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemonData.id}.gif`}
           alt={pokemonData.name}
         />
+
         <div className="my-4">
           <p className="truncate text-2xl">{capitalize(pokemonData.name)}</p>
         </div>
+
         <div className="pokemon-card-body">
           <p className="pokemon-card-id">{pokemonData.id}</p>
+
           <div className="pokemon-card-wrapper">
-            {pokemonData.types.map((type) => (
+            <p
+              className={`pokemon-card-type ${getType(
+                pokemonData.types[0].type.name
+              )}`}
+            >
+              {pokemonData.types[0].type.name}
+            </p>
+
+            {pokemonData.types[1]?.type.name && (
               <p
-                className={`pokemon-card-type ${getType(type.type.name)}`}
-                key={type + pokemonData.id}
+                className={`pokemon-card-type ${getType(
+                  pokemonData.types[1].type.name
+                )}`}
               >
-                {type.type.name}
+                {pokemonData.types[1].type.name}
               </p>
-            ))}
+            )}
+
           </div>
         </div>
       </div>
