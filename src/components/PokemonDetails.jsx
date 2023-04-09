ccf46cd14609bd025323d2a53020b42f48494b44
Kevin Feitosa <75489626+kevinLyf@users.noreply.github.com>
2023-04-09 17:05:55 -0300

    feat(card pokemon unkown): create a component

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import capitalize from "../hooks/capitalize";
 import getType from "@/hooks/getType";
+import UnknownPokemon from "./UnkownPokemon";
 
 function PokemonDetails({ pokemonName, isSearch, search, setIsSearch }) {
   const [pokemonData, setPokemonData] = useState(null);
@@ -17,26 +18,7 @@ function PokemonDetails({ pokemonName, isSearch, search, setIsSearch }) {
   }, [pokemonName, isSearch]);
 
   if (!pokemonData) {
-    return (
-      <div className="pokemon-card unknown">
-        <img
-          className="w-auto h-28"
-          src="https://o.remove.bg/downloads/9f9f3e53-d63f-4077-8c63-945be92fe1aa/image-removebg-preview.png"
-          alt="unknown pokemon"
-        />
-        <div className="my-4">
-          <p className="truncate text-2xl">unknown</p>
-        </div>
-
-        <div className="pokemon-card-body">
-          <p className="pokemon-card-id">unknown</p>
-
-          <div className="pokemon-card-wrapper">
-            <p className="pokemon-card-type unknown">unknown</p>            
-          </div>
-        </div>
-      </div>
-    );
+    return <UnknownPokemon />;
   }
 
   return (
