0653c513b929d3720286e5b1a194aa05252c4b44
Kevin Feitosa <75489626+kevinLyf@users.noreply.github.com>
2023-04-08 23:15:28 -0300

    feat(pages): create navbar, card and about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+
+    return(
+        <>
+            <h1>About</h1>
+        </>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,47 @@
+import Image from "next/image";
 
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemons = 23;
+  const api = "https://pokeapi.co/api/v2/pokemon";
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }) {
   return (
     <>
-     <h1>Hello, World!</h1>
+      <div className="mx-auto max-w-7xl grid place-items-center gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4 sm:px-6 lg:px-8">
+        {pokemons.map((pokemon) => (
+          <div
+            className="w-11/12 sm:w-full h-full flex items-center justify-center flex-col p-4 bg-red-500 text-white rounded"
+            key={pokemon.id}
+          >
+            <div className="flex align-middle justify-center">
+              <Image
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                width="130"
+                height="130"
+                alt={pokemon.name}
+              />
+            </div>
+            <div className="flex gap-4 items-center justify-center">
+              <span className="text-2xl">{pokemon.id}</span>
+              <h3 className="text-2xl">{pokemon.name}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
